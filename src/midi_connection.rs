2fed837fdeb7fac03a46c198d2029e3798f1985b
julien <julien@eudine.fr>
2025-04-18 19:05:22 +0200

    Midi connection updated with blocking function

--- src/midi_connection.rs
+++ src/midi_connection.rs
@@ -1,4 +1,9 @@
-use stm32f4xx_hal::{pac::USART1, prelude::*, serial::Tx};
+use stm32f4xx_hal::{
+    block,
+    pac::{dma1::ST, i2c1::cr1::START, USART1},
+    prelude::*,
+    serial::Tx,
+};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -28,39 +33,33 @@ const CC: u8 = 0xB0;
 impl mseq::MidiOut for MidiOut {
     type Error = MidiError;
     fn send_start(&mut self) -> Result<(), MidiError> {
-        self.tx.write(START).map_err(|_| MidiError::Write)
+        wb(&mut self.tx, &[START])
     }
     fn send_continue(&mut self) -> Result<(), MidiError> {
-        self.tx.write(CONTINUE).map_err(|_| MidiError::Write)
+        wb(&mut self.tx, &[CONTINUE])
     }
     fn send_stop(&mut self) -> Result<(), MidiError> {
-        self.tx.write(STOP).map_err(|_| MidiError::Write)
+        wb(&mut self.tx, &[STOP])
     }
     fn send_clock(&mut self) -> Result<(), MidiError> {
-        self.tx.write(CLOCK).map_err(|_| MidiError::Write)
+        wb(&mut self.tx, &[CLOCK])
     }
     fn send_note_on(&mut self, channel_id: u8, note: u8, velocity: u8) -> Result<(), MidiError> {
-        self.tx
-            .write(NOTE_ON | (channel_id - 1))
-            .map_err(|_| MidiError::Write)?;
-        self.tx.write(note).map_err(|_| MidiError::Write)?;
-        self.tx.write(velocity).map_err(|_| MidiError::Write)?;
-        Ok(())
+        wb(&mut self.tx, &[NOTE_ON | (channel_id - 1), note, velocity])
     }
     fn send_note_off(&mut self, channel_id: u8, note: u8) -> Result<(), MidiError> {
-        self.tx
-            .write(NOTE_OFF | (channel_id - 1))
-            .map_err(|_| MidiError::Write)?;
-        self.tx.write(note).map_err(|_| MidiError::Write)?;
-        self.tx.write(0).map_err(|_| MidiError::Write)?;
-        Ok(())
+        wb(&mut self.tx, &[NOTE_OFF | (channel_id - 1), note, 0])
     }
     fn send_cc(&mut self, channel_id: u8, parameter: u8, value: u8) -> Result<(), MidiError> {
-        self.tx
-            .write(CC | (channel_id - 1))
-            .map_err(|_| MidiError::Write)?;
-        self.tx.write(parameter).map_err(|_| MidiError::Write)?;
-        self.tx.write(value).map_err(|_| MidiError::Write)?;
-        Ok(())
+        wb(&mut self.tx, &[CC | (channel_id - 1), parameter, value])
     }
+}
+fn wb<U>(tx: &mut Tx<U>, bytes: &[u8]) -> Result<(), MidiError>
+where
+    U: stm32f4xx_hal::serial::Instance,
+{
+    for &b in bytes {
+        block!(tx.write(b)).map_err(|_| MidiError::Write)?;
+    }
+    Ok(())
 }
